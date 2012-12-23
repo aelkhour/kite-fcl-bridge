c4676f9e96b86691657601ec473eec30a96b6a3d
Antonio El Khoury <aelkhour@laas.fr>
2012-12-23 14:35:59 +0100

    Finish changing package name.

--- include/kws/fcl/fwd.hh
+++ include/kws/fcl/fwd.hh
@@ -1,19 +1,19 @@
 // Copyright (C) 2012 by Antonio El Khoury, CNRS.
 //
-// This file is part of the kws-fcl-bridge.
+// This file is part of the kite-fcl-bridge.
 //
-// kws-fcl-bridge is free software: you can redistribute it and/or
+// kite-fcl-bridge is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
 //
-// kws-fcl-bridge is distributed in the hope that it will be useful,
+// kite-fcl-bridge is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public
-// License along with kws-fcl-bridge.  If not, see
+// License along with kite-fcl-bridge.  If not, see
 // <http://www.gnu.org/licenses/>.
 
 /// \brief Forward declarations.
