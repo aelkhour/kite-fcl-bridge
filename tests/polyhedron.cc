97548a4fcabc004ad90deff2474316922bf3d97d
Antonio El Khoury <aelkhour@laas.fr>
2012-12-25 15:04:18 +0100

    Add test for polyhedron.

---
+++ tests/polyhedron.cc
@@ -0,0 +1,33 @@
+// Copyright (C) 2012 by Antonio El Khoury, CNRS.
+//
+// This file is part of the kite-fcl-bridge.
+//
+// kite-fcl-bridge is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// kite-fcl-bridge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with kite-fcl-bridge.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#define BOOST_TEST_MODULE body
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+#include <kite-fcl-bridge/polyhedron.hh>
+
+using boost::test_tools::output_test_stream;
+
+BOOST_AUTO_TEST_CASE (polyhedron)
+{
+  kite_fcl_bridge::PolyhedronShPtr polyhedron
+    = kite_fcl_bridge::Polyhedron::create ("polyhedron");
+  BOOST_CHECK_EQUAL(!polyhedron, 0);
+}
